781664e4416838bc2320be8b2d1cda8d596f2f22
Nagaraju <nagaraju.kukudala@gmail.com>
2016-11-19 13:12:52 +0530

    Add files via upload

---
+++ app.js
@@ -0,0 +1,15 @@
+var name = "Nagaraju Kukudala"; // Declaring Name
+var age = 25;//Declaring Age
+var dateOfBirth = "20-02-1991"; // Declaring Date of Birth
+var placeOfBirth = "Telangana"; // Declaring Place of Birth
+myDetails();// Calling myDetails function
+// function for Console.log
+function myDetails(){
+console.log("Name :" + name);  
+console.log("Age :" + age);
+console.log("Date Of Birth :" + dateOfBirth);
+console.log("Place Of Birth :" + placeOfBirth);
+    
+    /* Console.log functions for printing name, 
+    age, date of birth and place of birth*/
+    }
